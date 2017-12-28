d4a0ce634e0bb384112256420f7f30e59b6d9279
Holy.Han <hanhongliang@smart-haier.com>
2017-12-28 09:55:31 +0800

    删除obj

--- SmartSale/library/src/main/obj/local/armeabi-v7a/objs/native-serialport-jni/serial.o.d
+++
@@ -1,2 +0,0 @@
-E:/Git/Android_APP_CONTROL/SmartSale/library/src/main/obj/local/armeabi-v7a/objs/native-serialport-jni/serial.o: \
-  SerialPort/serial.c
--- SmartSale/library/src/main/obj/local/x86/objs/native-serialport-jni/serial.o.d
+++
@@ -1,2 +0,0 @@
-E:/Git/Android_APP_CONTROL/SmartSale/library/src/main/obj/local/x86/objs/native-serialport-jni/serial.o: \
-  SerialPort/serial.c

53614de616b3f5301fd01d69590ac022ece28f92
Holy.Han <hanhongliang@smart-haier.com>
2018-01-08 15:19:13 +0800

    调整语音的播放限制

--- SmartSale/library/src/main/java/com/haiersmart/library/MediaPlayer/PlayFixedVoice.java
+++ SmartSale/library/src/main/java/com/haiersmart/library/MediaPlayer/PlayFixedVoice.java
@@ -2,6 +2,9 @@ package com.haiersmart.library.MediaPlayer;
 
 import android.content.Context;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Copyright 2017, Smart Haier. All rights reserved.
  * Description:
@@ -32,46 +35,83 @@ public class PlayFixedVoice {
     public static final int PROMPT = 0;
     public static final int YSM3A1T = 1;
 
+    private static boolean mIsPlay = false;
+    private static final int mDelayTimeMill = 5000;
+
 
     /**
      * 基本播放，可指定设备，指定声音播放
+     *
      * @param context
-     * @param device 设备指定 PROMPT YSM3A1T
-     * @param voice 音频文件 OPEN SELECT FINAL PAY
+     * @param device  设备指定 PROMPT YSM3A1T
+     * @param voice   音频文件 OPEN SELECT FINAL PAY
      */
-    public static void playVoice(Context context,int device,int voice){
-        if(device == PROMPT){
-            PlayPromptVoice.playVoice(context,voice);
-        }else if(device == YSM3A1T){
-            PlayYSM3A1TVoice.playVoiceOnce(voice);
+    public static void playVoice(Context context, int device, int voice) {
+        if (!mIsPlay) {
+            mIsPlay = true;
+            timerDelay();
+            if (device == PROMPT) {
+                PlayPromptVoice.playVoice(context, voice);
+            } else if (device == YSM3A1T) {
+                PlayYSM3A1TVoice.playVoiceOnce(voice);
+            }
         }
     }
 
     /**
      * 播放assets内文件
+     *
      * @param context
-     * @param voice 音频文件 OPEN SELECT FINAL PAY
+     * @param voice   音频文件 OPEN SELECT FINAL PAY
      */
-    public static void playVoice(Context context,int voice){
-        PlayPromptVoice.playVoice(context,voice);
+    public static void playVoice(Context context, int voice) {
+        if (!mIsPlay) {
+            mIsPlay = true;
+            timerDelay();
+            PlayPromptVoice.playVoice(context, voice);
+        }
     }
 
     /**
      * 播放assets以外的设备音频
+     *
      * @param device 设备指定 YSM3A1T
-     * @param voice 音频文件 OPEN SELECT FINAL PAY
+     * @param voice  音频文件 OPEN SELECT FINAL PAY
      */
-    public static void playVoice(int device,int voice){
-        if(device == YSM3A1T){
-            PlayYSM3A1TVoice.playVoiceOnce(voice);
+    public static void playVoice(int device, int voice) {
+        if (!mIsPlay) {
+            mIsPlay = true;
+            timerDelay();
+            if (device == YSM3A1T) {
+                PlayYSM3A1TVoice.playVoiceOnce(voice);
+            }
         }
     }
 
     /**
      * 播放默认设备YSM3A1T的音频
+     *
      * @param voice 音频文件 OPEN SELECT FINAL PAY
      */
-    public static void playVoice(int voice){
-        PlayYSM3A1TVoice.playVoiceOnce(voice);
+    public static void playVoice(int voice) {
+        if (!mIsPlay) {
+            mIsPlay = true;
+            timerDelay();
+            PlayYSM3A1TVoice.playVoiceOnce(voice);
+        }
     }
+
+    private static void timerDelay() {
+        final Timer timer = new Timer("play voice delay");
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                mIsPlay = false;
+                timer.cancel();
+                timer.purge();
+            }
+        }, mDelayTimeMill);
+    }
+
 }
